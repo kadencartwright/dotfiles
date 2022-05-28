c12f864f0f4647bde236031c2271f78285abd115
kadencartwright <cartwrightkaden@gmail.com>
2022-05-28 13:47:30 -0500

    added rust and additional p10k config

--- p10k/install.sh
+++ p10k/install.sh
@@ -2,3 +2,5 @@
 cp '../artifacts/MesloLGS NF Regular' ~/Library/Fonts
 ## now install p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+ln -sf ~/dotfiles/p10k/.p10k.zsh ~/.p10k.zsh
