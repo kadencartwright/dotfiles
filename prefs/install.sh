6ee96d44e05156f237d4a76fa802d3db3773eb80
kadencartwright <cartwrightkaden@gmail.com>
2022-05-28 13:33:00 -0500

    added keyrepeat and oh-my-zsh

---
+++ prefs/install.sh
@@ -0,0 +1,4 @@
+#keyrepeat prefs
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 13
