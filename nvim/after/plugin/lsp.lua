32497ba08b4d6401fcc8cf50c7ae066745af2c1b
Kaden Cartwright <cartwrightkaden@gmail.com>
2024-01-13 21:51:33 -0600

    update fuzzel

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,15 +1,18 @@
 local lsp = require("lsp-zero")
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
 
-lsp.preset("recommended")
-
-lsp.ensure_installed({
 	'tsserver',
 	'eslint',
 	'lua_ls',
 	'rust_analyzer',
     'gopls',
     'bashls'
+      },
 })
+lsp.preset("recommended")
+
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
 	settings = {
@@ -38,7 +41,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 
-lsp.setup_nvim_cmp({
+cmp.setup({
 	mapping = cmp_mappings
 })
 
@@ -68,82 +71,6 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
    	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-    if client.name == "omnisharp" then
-        client.server_capabilities.semanticTokensProvider = {
-            full = vim.empty_dict(),
-            legend = {
-                tokenModifiers = { "static_symbol" },
-                tokenTypes = {
-                    "comment",
-                    "excluded_code",
-                    "identifier",
-                    "keyword",
-                    "keyword_control",
-                    "number",
-                    "operator",
-                    "operator_overloaded",
-                    "preprocessor_keyword",
-                    "string",
-                    "whitespace",
-                    "text",
-                    "static_symbol",
-                    "preprocessor_text",
-                    "punctuation",
-                    "string_verbatim",
-                    "string_escape_character",
-                    "class_name",
-                    "delegate_name",
-                    "enum_name",
-                    "interface_name",
-                    "module_name",
-                    "struct_name",
-                    "type_parameter_name",
-                    "field_name",
-                    "enum_member_name",
-                    "constant_name",
-                    "local_name",
-                    "parameter_name",
-                    "method_name",
-                    "extension_method_name",
-                    "property_name",
-                    "event_name",
-                    "namespace_name",
-                    "label_name",
-                    "xml_doc_comment_attribute_name",
-                    "xml_doc_comment_attribute_quotes",
-                    "xml_doc_comment_attribute_value",
-                    "xml_doc_comment_cdata_section",
-                    "xml_doc_comment_comment",
-                    "xml_doc_comment_delimiter",
-                    "xml_doc_comment_entity_reference",
-                    "xml_doc_comment_name",
-                    "xml_doc_comment_processing_instruction",
-                    "xml_doc_comment_text",
-                    "xml_literal_attribute_name",
-                    "xml_literal_attribute_quotes",
-                    "xml_literal_attribute_value",
-                    "xml_literal_cdata_section",
-                    "xml_literal_comment",
-                    "xml_literal_delimiter",
-                    "xml_literal_embedded_expression",
-                    "xml_literal_entity_reference",
-                    "xml_literal_name",
-                    "xml_literal_processing_instruction",
-                    "xml_literal_text",
-                    "regex_comment",
-                    "regex_character_class",
-                    "regex_anchor",
-                    "regex_quantifier",
-                    "regex_grouping",
-                    "regex_alternation",
-                    "regex_text",
-                    "regex_self_escaped_character",
-                    "regex_other_escape",
-                },
-            },
-            range = true,
-        }
-    end
 end)
 
 lsp.setup()
