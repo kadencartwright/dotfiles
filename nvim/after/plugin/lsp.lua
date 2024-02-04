2de009a89d3633a181248720b73e6970357f7576
Kaden Cartwright <cartwrightkaden@gmail.com>
2024-02-04 00:02:25 -0600

    added K mapping to vim.lsp.buf.hover

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -29,6 +29,7 @@ local on_attach =
 
         vim.keymap.set('n', '<leader>qf', quickfix, opts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
