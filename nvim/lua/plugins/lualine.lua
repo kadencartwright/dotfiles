d92e339ff77cadc33a018937ec7ba6e74443f7cd
Kaden Cartwright <cartwrightkaden@gmail.com>
2025-06-09 21:24:48 -0500

    update lualine to remove branch names

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,6 +8,15 @@ return {
 			theme = "onedark",
 			component_separators = "|",
 			section_separators = "",
+			max_width = 40,
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "filename" },
+			lualine_c = { "diff", "diagnostics" },
+			lualine_x = { "fileformat" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
 		},
 	},
 }
