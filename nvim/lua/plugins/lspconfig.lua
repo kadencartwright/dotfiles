c4dc0c02703db7d785de6f555d336ec5ef8b1d4e
Kaden Cartwright <cartwrightkaden@gmail.com>
2025-07-13 21:39:13 -0500

    update nvim config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -231,7 +231,6 @@ return {
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				ts_ls = {},
 				--
 
 				jsonls = {
