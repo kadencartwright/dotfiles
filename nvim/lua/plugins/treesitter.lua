df2f3f842815320f7efdea868a67a0f01f4cadc6
Kaden Cartwright <cartwrightkaden@gmail.com>
2026-06-02 10:27:45 -0500

    update config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,11 @@ return {
 	{ -- Highlight, edit, and navigate code
 
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
+		config = function()
+			local languages = {
 				"bash",
 				"tsx",
 				"diff",
@@ -14,25 +14,38 @@ return {
 				"lua",
 				"typescript",
 				"javascript",
+				"json",
+				"json5",
 				"luadoc",
 				"markdown",
 				"markdown_inline",
 				"query",
 				"vim",
 				"vimdoc",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+			}
+
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+			require("nvim-treesitter").install(languages):wait(300000)
+
+			vim.treesitter.language.register("bash", "zsh")
+			vim.treesitter.language.register("json5", "jsonc")
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true }),
+				pattern = "*",
+				callback = function()
+					local ok = pcall(vim.treesitter.start)
+					if not ok then
+						return
+					end
+
+					if vim.bo.filetype ~= "ruby" then
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+		end,
 	},
 }
