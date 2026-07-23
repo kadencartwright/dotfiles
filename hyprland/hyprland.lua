b02c92cffb53715cf8e60fa631f969df9c3ac3cf
Kaden Cartwright <cartwrightkaden@gmail.com>
2026-07-23 12:15:09 -0500

    optimize hyprland perf

--- hyprland/hyprland.lua
+++ hyprland/hyprland.lua
@@ -66,7 +66,7 @@ require("monitors")
 
 hl.config({
 	cursor = {
-		no_hardware_cursors = true,
+		no_hardware_cursors = false,
 	},
 
 	xwayland = {
@@ -151,8 +151,6 @@ hl.curve("myBezier", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.05 } 
 hl.animation({ leaf = "workspaces", enabled = false })
 hl.animation({ leaf = "windows", enabled = true, speed = 6, bezier = "myBezier", style = "popin 60%" })
 hl.animation({ leaf = "windowsOut", enabled = true, speed = 6, bezier = "default", style = "popin 90%" })
-hl.animation({ leaf = "border", enabled = true, speed = 1, bezier = "default" })
-hl.animation({ leaf = "borderangle", enabled = true, speed = 1, bezier = "default" })
 hl.animation({ leaf = "fade", enabled = true, speed = 3, bezier = "default" })
 
 ------------------
